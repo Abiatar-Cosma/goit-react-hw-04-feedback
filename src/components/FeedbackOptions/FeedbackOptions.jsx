4cd7423f63a5a585572e3baed90b13e69ca7dd49
Abiatar Cosma <abiatar.cosma@yahoo.com>
2025-02-01 20:28:16 +0100

    feedback-widget-refactorizat

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
-
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div className={styles.buttons}>
-    {options.map((option) => (
+    {options.map(option => (
       <button
         key={option}
         onClick={() => onLeaveFeedback(option)}
         className={styles.button}
+        type="button"
       >
         {option}
       </button>
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Notification.module.css';
 
-const Notification = ({ message }) => (
-  <p className={styles.notification}>{message}</p>
-);
+const Notification = ({ message }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Setăm vizibilitatea la true după ce componenta este montată
+    setIsVisible(true);
+  }, []);
+
+  return (
+    <p className={`${styles.notification} ${isVisible ? styles.fadeIn : ''}`}>
+      {message}
+    </p>
+  );
+};
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
